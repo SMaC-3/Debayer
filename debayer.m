3218052d1d51e511303d6c08011568806ba6ec00
Josh King <josh.king@monash.edu>
2020-10-12 14:34:13 +1100

    Add background subtraction

--- debayer.m
+++ debayer.m
@@ -1,6 +1,13 @@
+close all; clear all
 % Dimenstions of .raw images. Assumed to be square
 cols = 2048;
 rows = cols;
+
+% Load background image file
+
+bg_file = uigetfile('*.raw','Select the background raw-file');
+bg_open = fopen(bg_file, 'r', 'l');
+bg_data = fread(bg_open, [cols, rows], '*uint16');
 
 % Create matrix of odd and even integers up to file dimensions
 even = 2*(1:cols/2);
@@ -11,31 +18,21 @@ odd = even-1;
 
 % Perform debayering and save separate colour channels as .tiff files with
 % colour prepended
-if iscell(fileNames) == 1
-    for i = 1:length(fileNames)
-        file = fileNames{i};
-        fin = fopen(file, 'r', 'l');
-        ima = fread(fin, [cols rows], '*uint16');
-        
-        oddOdd = ima(odd, odd);
-        oddEven = ima(odd, even);
-        evenEven = ima(even, even);
-        evenOdd = ima(even, odd);
-        
-        imwrite(oddEven, strcat("red_", file(1:end-4), ".tiff"))
-        imwrite(evenOdd, strcat("blue_", file(1:end-4), ".tiff"))
-        %     imwrite(oddOdd, strcat("green1_", file(1:end-4), ".tiff"))
-        %     imwrite(evenEven, strcat("green2_", file(1:end-4), ".tiff"))
-    end
-else 
-    file = fileNames;
+
+if iscell(fileNames) == 0 
+    fileNames = {fileNames};
+end
+
+for i = 1:length(fileNames)
+    file = fileNames{i};
     fin = fopen(file, 'r', 'l');
     ima = fread(fin, [cols rows], '*uint16');
     
-    oddOdd = ima(odd, odd);
-    oddEven = ima(odd, even);
-    evenEven = ima(even, even);
-    evenOdd = ima(even, odd);
+    ima_sub = ima - bg_data;
+    oddOdd = ima_sub(odd, odd);
+    oddEven = ima_sub(odd, even);
+    evenEven = ima_sub(even, even);
+    evenOdd = ima_sub(even, odd);
     
     imwrite(oddEven, strcat("red_", file(1:end-4), ".tiff"))
     imwrite(evenOdd, strcat("blue_", file(1:end-4), ".tiff"))
@@ -43,17 +40,26 @@ else
     %     imwrite(evenEven, strcat("green2_", file(1:end-4), ".tiff"))
 end
 
+
 % Show each colour channel of final image
-figure(1)
-imshow(oddOdd)
-
-figure(2)
-imshow(oddEven)
-title('red')
-
-figure(3)
-imshow(evenEven)
-
-figure(4)
-imshow(evenOdd)
-title('blue')
+% figure(1)
+% imshow(oddOdd)
+% 
+% figure(2)
+% imshow(oddEven)
+% figure(2)
+% title('red')
+% 
+% figure(3)
+% imshow(evenEven)
+% 
+% figure(4)
+% imshow(evenOdd)
+% figure(4)
+% title('blue')
+% 
+% figure(5)
+% imshow(ima_sub);
+% 
+% figure(6)
+% imshow(ima);
